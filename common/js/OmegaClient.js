eccd40b68d3999a87c91089a813c2174e4574e85
Jonny Fillmore <git@spamriot.com>
2012-02-28 16:07:41 -0800

    added del as alternative to delete for JS OmegaClient restful method

--- common/js/OmegaClient.js
+++ common/js/OmegaClient.js
@@ -455,9 +455,10 @@
 				return om_client.request('PUT', api, params, callback, fail_callback, args);
 			};
 
-			om_client['delete'] = function (api, params, callback, fail_callback, args) {
+			om_client.del = function (api, params, callback, fail_callback, args) {
 				return om_client.request('DELETE', api, params, callback, fail_callback, args);
 			};
+			om_client['delete'] = om.client.del;
 
 			om_client.request = function (method, api, params, callback, fail_callback, args) {
 				var ajax, ajax_args;
