d3f368de73b34162f8e5f0d8dc3438b8b294f9db
Jonny Fillmore <jonny.fillmore@uk2group.com>
2012-07-03 00:27:25 +0000

    auto write headers on getting output stream, unless requested otherwise

--- servers/php/classes/Omega.class.php
+++ servers/php/classes/Omega.class.php
@@ -117,10 +117,14 @@ class Omega extends OmegaLib {
     }
 
     /** Returns a reference to an output stream that can be written to for providing an API response. */
-    public function _get_output_stream() {
+    public function _get_output_stream($write_headers = true) {
         global $om;
         // end output buffering, since we're taking over
         $spillage = $this->_flush_ob(false);
+        // note that headers MUST be sent before writing to the output stream, but you could concievably want to delay just a bit longer
+        if ($write_headers) {
+            $this->_write_headers();
+        }
         // take note of anything that slipped out prematurely (e.g. warnings)
         if (strlen($spillage) > 0 && ! $this->in_production()) {
             $this->response->set_spillage($spillage);
@@ -147,7 +151,6 @@ class Omega extends OmegaLib {
             // send headers before getting the output stream
             $this->response->set_encoding('raw');
             $this->response->header('Content-Type', mime_content_type($file));
-            $this->_write_headers();
             // write chunks
             $os = $this->_get_output_stream();
             $offset = 0;
